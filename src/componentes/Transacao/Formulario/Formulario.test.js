b71803b2bbfb3e31b1e1fca39e7b6e9185da7ba8
MatheusSousaOSF <mscorreia95@gmail.com>
2023-02-06 04:23:03 +0000

    Test selection using userEvent

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import  Formulario  from './index';
+import Formulario from './index';
 
-test('Deve redenrizar um campo de imput', () => {
+/* test('Deve redenrizar um campo de imput', () => {
   render(<Formulario />);
   const campoDeTexto = screen.getByPlaceholderText('Digite um valor');
   expect(campoDeTexto).toBeInTheDocument();
@@ -17,6 +17,20 @@ test('Deve redenrizar um campo de imput com um número', () => {
 test('Deve redenrizar um campo de imput que pode ser preenchido', () => {
   render(<Formulario />);
   const campoDeTexto = screen.getByPlaceholderText('Digite um valor');
-  userEvent.type(campoDeTexto, '50')
+  userEvent.type(campoDeTexto, '50');
   expect(campoDeTexto).toHaveValue(50);
+}); */
+
+test('Simular seleção de transação', () => {
+  render(<Formulario />);
+  const campoDeSelecao = screen.getByTestId('select-opcoes');
+  userEvent.selectOptions(campoDeSelecao, 'Depósito');
+  expect(screen.getByRole('option', { name: 'Depósito' }).selected).toBe(true);
+  expect(screen.getByRole('option', { name: 'Transferência' }).selected).toBe(
+    false
+  );
+  expect(
+    screen.getByRole('option', { name: 'Selecione um tipo de transação' })
+      .selected
+  ).toBe(false);
 });
